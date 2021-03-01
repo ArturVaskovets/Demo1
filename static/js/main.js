c3ae65a2be71e94f067fa9dee5c939c11cc64ac1
ArturVaskovets <artik2607@gmail.com>
2021-03-01 16:19:20 +0100

    Added animator

--- static/js/main.js
+++ static/js/main.js
@@ -6,4 +6,23 @@ $(document).ready(function(){
 			scrollTop: $(target).offset().top
 		}, 1000);
 	});
+
+	animator();
+	$(window).scroll(animator);
 });
+
+function animator () {
+	var elements = $(".common_section .section_inner");
+	var window_height = parseInt($(window).height());
+	var window_trigger = window_height / 2 + 300;
+	var actual_position = parseInt($(window).scrollTop());
+
+	elements.each(function () {
+		var element_top = parseInt($(this).offset().top);
+		if ((actual_position + window_trigger) > element_top) {
+			$(this).removeClass("fadeOutUp").addClass("fadeInUp");
+		} else {
+			$(this).removeClass("fadeInUp").addClass("fadeOutUp");
+		}
+	});
+}
