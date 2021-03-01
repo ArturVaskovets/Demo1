3e09d75720a628db53f249f16908fdc0996cbb2d
ArturVaskovets <artik2607@gmail.com>
2021-03-01 20:33:02 +0100

    Added floating_menu

--- static/js/main.js
+++ static/js/main.js
@@ -7,11 +7,13 @@ $(document).ready(function(){
 		}, 1000);
 	});
 
-	animator();
-	$(window).scroll(animator);
+	sectionAnimator();
+	floatingMenuAnimator();
+	$(window).scroll(sectionAnimator);
+	$(window).scroll(floatingMenuAnimator);
 });
 
-function animator () {
+function sectionAnimator () {
 	var elements = $(".common_section");
 	var window_height = parseInt($(window).height());
 	var window_trigger = window_height / 2 + 100;
@@ -21,8 +23,28 @@ function animator () {
 		var element_top = parseInt($(this).offset().top);
 		if ((actual_position + window_trigger) > element_top) {
 			$(this).find(".section_inner").removeClass("fadeOutUp").addClass("fadeInUp");
+			setActiveSection($(this).attr("id"));
 		} else {
 			$(this).find(".section_inner").removeClass("fadeInUp").addClass("fadeOutUp");
 		}
 	});
+}
+
+function floatingMenuAnimator() {
+	var element = $(".floating_menu");
+	var window_height = parseInt($(window).height());
+	var actual_position = parseInt($(window).scrollTop());
+
+	if (actual_position > window_height) {
+		element.addClass("active");
+	} else {
+		element.removeClass("active");
+	}
+}
+
+function setActiveSection(id) {
+	var element = $(".floating_menu");
+
+	element.find(`.menu_item[href='#${id}']`).addClass("active");
+	element.find(`.menu_item[href!='#${id}']`).removeClass("active");
 }
